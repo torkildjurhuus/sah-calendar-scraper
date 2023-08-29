55937ee1ff169f165023151d31fb828cb367ab39
ci-bb-user <ci-user@globetracker.com>
2023-08-29 23:20:38 +0100

    feat: updated initialization of firebase connection

--- scraper.js
+++ scraper.js
@@ -2,12 +2,13 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const admin = require('firebase-admin');
 
-const serviceAccount = require(process.env.FIREBASE_ADMIN_CREDENTIALS);
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIALS);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: process.env.FIREBASE_DB_URL
 });
+
 
 const db = admin.database();
 
